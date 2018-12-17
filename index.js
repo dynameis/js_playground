98b230b00a918a83a41f66970af8c57cd52316f8
陳信羽 <evaimpact@outlook.com>
2018-12-17 22:41:24 +0800

    monaco

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 (() => {
+  require.config({ paths: { 'vs': 'monaco-editor/min/vs' } });
+  let editor;
+  require(['vs/editor/editor.main'], function () {
+    const container = document.getElementById('editor');
+    editor = monaco.editor.create(container, {
+      value: 'gui.output = \'hello world\'',
+      language: 'javascript',
+      theme: 'vs-dark', minimap: {
+        enabled: false
+      },
+      automaticLayout: true
+    });
+  });
   const inputElem = document.querySelector('#input');
   const outputElem = document.querySelector('#output');
   const errorElem = document.querySelector('#error');
@@ -15,35 +28,45 @@
   function padLeft(nr, n, str) {
     return Array(n - String(nr).length + 1).join(str || '0') + nr;
   }
-  document.querySelector('#execute').addEventListener('click', () => {
+  var excute = () => {
+    console.log('code excuting..');
     gui.output = '';
     errorElem.textContent = '';
-    fetch('./main/main.js?_=' + new Date().valueOf())
-      .then(res => res.text())
-      .then(sc => {
-        const func = new Function('gui', sc);
-        try {
-          func(gui);
-        } catch (ex) {
-          let errMsg = ex.toString();
-          const er = /<anonymous>:(\d+):(\d+)/.exec(ex.stack);
-          if (er.length) {
-            const sp = sc.split('\n').map((v, i) => padLeft(i, 4) + '. ' + v);
-            let nl = '';
-            for (let i = 0, c = parseInt(sp[2], 10); i < c; i++) {
-              nl += ' ';
-            }
-            const errIndex = er[1] - 2;
-            sp.splice(errIndex, 0, nl + '    ^^^^^');
-            errMsg +=
-              '\n\n' +
-              sp
-                .filter((v, i) => i > errIndex - 3 && i < errIndex + 3)
-                .join('\n');
+    const sc = editor.getValue();
+    let func;
+    try {
+      func = new Function('gui', sc);
+      try {
+        func(gui);
+      } catch (ex) {
+        let errMsg = ex.toString();
+        const er = /<anonymous>:(\d+):(\d+)/.exec(ex.stack);
+        if (er.length) {
+          const sp = sc.split('\n').map((v, i) => padLeft(i + 1, 4) + '. ' + v);
+          let nl = '';
+          for (let i = 0, c = parseInt(sp[2], 10); i < c; i++) {
+            nl += ' ';
           }
-          console.error(errMsg);
-          errorElem.textContent = errMsg;
+          const errIndex = er[1] - 2;
+          sp.splice(errIndex, 0, nl + '    ^^^^^');
+          errMsg +=
+            '\n\n' +
+            sp
+              .filter((v, i) => i > errIndex - 3 && i < errIndex + 3)
+              .join('\n');
         }
-      });
-  });
+        console.error(errMsg);
+        errorElem.textContent = errMsg;
+      }
+    } catch (ex) {
+      errorElem.textContent = ex;
+    }
+
+  };
+  document.querySelector('#execute').addEventListener('click', excute);
+  inputElem.addEventListener('keypress', e => {
+    if (e.which == 13 || e.keyCode == 13) {
+      excute();
+    }
+  })
 })();
